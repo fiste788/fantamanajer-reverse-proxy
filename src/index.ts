83ebcba2692920952ff33a89032172e2e1c07c76
Stefano Sonzogni <stefano788@gmail.com>
2024-10-14 12:59:50 +0200

    improve logging

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,6 @@ export default {
 		// e.g. https://domain.com/blog/page
 		var url = new URL(request.url);
 
-		// Construct the cache key from the cache URL
-		const cacheKey = new Request(url.toString(), request);
-		const cache = caches.default;
-
-		// Check whether the value is already available in the cache
-		// if not, you will need to fetch it from R2, and store it in the cache
-		// for future access
-		let response = await cache.match(cacheKey);
-
-		if (response) {
-			console.log(`Cache hit for: ${request.url}.`);
-			return response;
-		}
-
-
 		// set hostname to the place we're proxying requests from
 		url.hostname = env.API_HOST;
 		url.protocol = env.API_PROTOCOL;
@@ -27,19 +12,11 @@ export default {
 
 		// remove the first occurence of /blog
 		// so it requests / of the proxy domain
-		url.pathname = url.pathname.replace('api/', '');
+		url.pathname = url.pathname.replace('/api', '');
 		// pass the modified url back to the request,
 
-		const date = new Date();
-		console.log(`Fetching: ${url.toString()}`);
-		response = await fetch(url, request);
-		//response = await onRequestGet(context, env);
-		console.log('Fetched' , Date.now() - date.getTime())
-		if (response.headers.has('Expires')) {
-			context.waitUntil(cache.put(cacheKey, response.clone()));
-		} else {
-			console.log('Not has expires');
-		}
+		let response = await fetch(url, request);
+		console.log('Fetched', response);
 		return response;
 	},
 };
